eecd087809c3e0bae9d41400e9d507ccf39ef513
Christopher Harris <56790192+decode-harris@users.noreply.github.com>
2021-08-27 17:12:49 +1000

    Updated Core Logic
    Renamed variables & reconfigured the script.

--- src/js/index.js
+++ src/js/index.js
@@ -1,107 +1,83 @@
-// selector [ gro ] : form input
-let gro = document.querySelector('#gro');
-
-// selector [ add ] : add button
-let add = document.querySelector('#add');
+let task = document.querySelector('#task'); // select the form [ task ] input element
+let addTask = document.querySelector('#addTask'); // select the form [ addTask ] button
 
 // event [ add ] : add item 
-add.addEventListener('click', (e)=>{
+addTask.addEventListener('click', (e)=>{
 
-    // prevent form submit
-    e.preventDefault();
+    e.preventDefault(); // prevent form submit
 
-    // validate form input
-    if (gro.value == '' || gro.value == null) {
+    // validate if form input is emtpy or null
+    if (task.value == '' || task.value == null) {
         
-        // input helper
-        alert('please fill in an item');
+        alert('please fill in an item'); // helper alert
         
-        // focus gro input
-        gro.focus();
+        task.focus(); // focus the input error
     }
-    else if (gro.value.length >= 20) {
+    // validate if task string length is equal or greater than 20 characters
+    else if (task.value.length >= 20) {
         
-        // input helper
-        alert('please use 20 characters or less');
+        alert('please use 20 characters or less'); // helper alert
 
-        // focus gro input
-        gro.focus();
+        gro.focus(); // focus the input error
     }
     else {
-        
-        // init app
-        runApp();
+        runApplication(); // init function [ Run Application ]
     }
+
+    task.value = ''; // reset form input element to default
+    task.focus(); // refocus the task input element on submit
 
 });
 
-// function [ runApp ] : start application
-function runApp() {
-
-    // selector [ out ] : output list
-    let output = document.querySelector('#out');
+// function [ Run Application ]
+runApplication = () => {
+    let list = document.querySelector('.list'); // select the list element
+    let items = document.createElement('li'); // create an items wrapper element
+    items.classList = 'items'; // assign items element with class name 'items'
     
-    // create list element for gro value 
-    let task = document.createElement('li');
+    let text = document.createElement('p'); // create a text data element
+    text.classList = 'text';
 
-    // create span element for buttons
+    // create a span container for items action buttons
     let span = document.createElement('span');
 
-    // create control buttons
-    let che = document.createElement('button');
-    let del = document.createElement('button');
+    // create the items action buttons
+    let itemsComplete = document.createElement('button');
+    let itemsDelete = document.createElement('button');
 
-    // assign gro value to new task element
-    task.innerHTML = gro.value;
+    text.innerHTML = task.value; // assign current task value to text element
     
-    // assign font awesome icons to buttons
-    che.innerHTML = '<i class="fas fa-check"></i>';
-    del.innerHTML = '<i class="fas fa-times"></i>';
-
-    // append buttons to span element
-    span.appendChild(che);
-    span.appendChild(del);
-
-    // append span to list element
-    task.appendChild(span);
-
-    // append list to unordered list element
-    output.appendChild(task);
-
-    // reset form input
-    gro.value = '';
+    itemsComplete.innerHTML = '<i class="fas fa-check"></i>'; // assign font awesome icons to buttons
+    itemsDelete.innerHTML = '<i class="fas fa-times"></i>'; // assign font awesome icons to buttons
     
-    // event [ che ] : check button
-    che.addEventListener('click', ()=>{
+    items.appendChild(text); // append the data text element to the items element
+    span.appendChild(itemsComplete); // append the action buttons to the span container
+    span.appendChild(itemsDelete); // append the action buttons to the span container
+    items.appendChild(span); // append the span container to data items element
+    list.appendChild(items); // append the data items element to the unordered list
+    
+    // click event [ Items Complete ] 
+    itemsComplete.addEventListener('click', ()=>{
 
         // validate checked list item
-        if (task.style.textDecoration != 'line-through') {
-            task.style.textDecoration = 'line-through';
-
-            // test
-            console.log('item checked');
+        if (text.style.textDecoration != 'line-through') {
+            text.style.textDecoration = 'line-through'; // apply style property text decoration to text element
+            console.log('Items Complete : True'); // test completed
+            items.classList += ' itemsComplete'; // add [ complete ] classname to items element
         } 
         // return list item to default
         else {
-            task.style.textDecoration = 'none';
+            text.style.textDecoration = 'none';// remove any text decoration styles from element
+            console.log('Items Complete : False'); // test style removal
+            items.classList.remove('itemsComplete');
 
-            // test
-            console.log('item un-checked');
+
         }
-        
     });
 
-    // event  [ del ] : delete button
-    del.addEventListener('click', ()=>{
-        
-        // remove task element
-        task.style.display = 'none';
-
-        // test
-        console.log('item deleted');
-
+    // click event [ Items Delete ] 
+    itemsDelete.addEventListener('click', ()=>{
+        items.style.display = 'none'; // remove the current items element
+        console.log('item deleted'); // test item removal
     });
-
-    // test
-    console.log(gro.value);
 }
